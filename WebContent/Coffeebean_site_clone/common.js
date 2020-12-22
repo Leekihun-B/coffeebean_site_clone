26cf6df64d890a6ed8ffe7252760b1d4d3041d97
Leekihun <yyou7704@naver.com>
2020-12-22 18:33:43 +0900

    3일차 body slick적용

---
+++ WebContent/Coffeebean_site_clone/common.js
@@ -0,0 +1,20 @@
+// MD'S PICK slick 효과
+$('.md_pick_1').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3800,
+    speed: 2800
+});
+
+// What's new 효과
+
+$('.what_new_1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 3600,
+    speed: 2800
+});
